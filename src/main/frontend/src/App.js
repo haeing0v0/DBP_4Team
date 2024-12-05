bbf31a0ea57b656a46ab265b155fef6d7c829caf
haein <162988398+haeing0v0@users.noreply.github.com>
2024-12-06 00:31:52 +0900

    부서 조회

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from './pages/main.js';
 import CheckIn from './pages/check_in.js';
-import Check from './pages/check.js';
+import Check from './pages/check.js'; 
+import Departments from './pages/departments.js';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/check-in" element={<CheckIn />} />
         <Route path="/check" element={<Check />} />
+        <Route path="/departments" element={<Departments />} />
       </Routes>
     </BrowserRouter>
   );
