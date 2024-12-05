4f79be295163958baa64209a1fb2a236757720a1
haein <162988398+haeing0v0@users.noreply.github.com>
2024-12-06 00:43:28 +0900

    직급 목록 UI 구현

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from './pages/main.js';
 import CheckIn from './pages/check_in.js';
 import Check from './pages/check.js'; 
-import Departments from './pages/departments.js';
+import Departments from './pages/departments.js'; 
+import Positions from './pages/positions.js';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/check-in" element={<CheckIn />} />
         <Route path="/check" element={<Check />} />
         <Route path="/departments" element={<Departments />} />
+        <Route path="/positions" element={<Positions />} />
       </Routes>
     </BrowserRouter>
   );
